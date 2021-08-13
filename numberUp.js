7ee49a5abb65bf11b99e32e54822338a938c820f
조은총 <whdmschd2@gmail.com>
2021-08-14 00:41:41 +0900

    two

--- numberUp.js
+++ numberUp.js
@@ -1,34 +1,3 @@
-/*const numberzz = document.querySelector(".number");
-const zero = 0;
-zero = zero + 25;
-
-function numberUp(){
-  console.log(numberzz);
-  numberzz.innerText = zero;
-}
-
-setInterval(numberUp, 3000);
-
-const numberzz = document.querySelector(".number");
-const zero = 0;
-zero = zero + 25;
-
-function numberUp(){
-  let number = 0;
-  if(number =>101){
-    numberzz.innerText = 100;
-  } else {
-    numberzz.innerText = number;
-}
-
-setInterval(numberUp, 3000);*/
-
-/*const numberzz = document.querySelector(".number");
-let zero;
-for (zero = 0; zero < 101; zero++) {
-  document.querySelector(".number").innerText = zero;
-}*/
-const yumdda = document.querySelector(".yumdda")
 let numberzz = 0;
 let interval = setInterval(function () { 
   for (i = 0; i < 1; i++) {
@@ -44,7 +13,15 @@ let interval = setInterval(function () {
 
 //setInterval (초기, 600)
 
+window.addEventListener("keydown", (e) => {
+  document.querySelector(".yumdda").style.display = "none";
+  document.querySelector(".lie").style.display = "none";
+  document.querySelector(".macbook").style.display = "none";
+});
 
-
-
+window.addEventListener("keyup", (e) => {
+  document.querySelector(".yumdda").style.display = "block";
+  document.querySelector(".lie").style.display = "block";
+  document.querySelector(".macbook").style.display = "block";
+});
 
